b9631e2e5b4591a67e805998046038eb92819bc4
samuel <samuel@DESKTOP-6I1D1FB>
2022-02-27 13:50:44 +0000

    Added service collection extensions

--- Injectable/Extensions/ServiceCollectionExtensions.cs
+++ Injectable/Extensions/ServiceCollectionExtensions.cs
@@ -1,65 +1,28 @@
-﻿//using Microsoft.Extensions.DependencyInjection;
-//using System.Reflection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
-//namespace Injectable.Extensions;
+namespace Injectable.Extensions;
 
-//public static class ServiceCollectionExtensions
-//{
-//    public static IServiceCollection AddInjectableServices(this IServiceCollection services, Assembly assembly)
-//    {
-//        var allTypes = assembly.GetTypes()
-//            .Where(x => x is { IsAbstract: false, IsClass: true, IsInterface: false });
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSingletons(this IServiceCollection services, IEnumerable<InjectableType> injectableTypes)
+    {
+        foreach (var type in injectableTypes)
+            services.AddSingleton(type.Service, type.Implementation);
+        return services;
+    }
 
-//        var injectableInstances = allTypes.SelectMany(x => GetInjectablesFromType(x));
+    public static IServiceCollection AddTransients(this IServiceCollection services, IEnumerable<InjectableType> injectableTypes)
+    {
+        foreach (var type in injectableTypes)
+            services.AddTransient(type.Service, type.Implementation);
+        return services;
+    }
 
-//        foreach (var instance in injectableInstances.Reverse().DistinctBy(x => x.Service))
-//        {
-//            Action<Type, Type> add = instance.Attribute.InjectionScope switch
-//            {
-//                InjectionScope.Singleton =>
-//                    (serviceType, implementationType) => services.AddSingleton(serviceType, implementationType),
-//                InjectionScope.Transient =>
-//                    (serviceType, implementationType) => services.AddTransient(serviceType, implementationType),
-//                InjectionScope.Scoped =>
-//                    (serviceType, implementationType) => services.AddScoped(serviceType, implementationType),
-//                _ => throw new NotImplementedException()
-//            };
-//            if (instance.Attribute.InjectionType.HasFlag(InjectionType.Inject))
-//                add(instance.Service, instance.Implementation);
-//            if (instance.Attribute.InjectionType.HasFlag(InjectionType.Concrete))
-//                add(instance.Implementation, instance.Implementation);
-//        }
-
-//        return services;
-//    }
-
-//    private static IEnumerable<InjectableType> GetInjectablesFromType(Type implementation)
-//    {
-//        IEnumerable<InjectableType> allBaseClasses(Type type)
-//        {
-//            yield return new InjectableType { Attribute = type.GetCustomAttribute<Inject>(), Implementation = implementation, Service = type };
-
-//            if (type.BaseType is not null)
-//            {
-//                foreach (var baseType in allBaseClasses(type.BaseType))
-//                    yield return baseType;
-//            }
-//        }
-//        var baseClasses = allBaseClasses(implementation);
-
-//        IEnumerable<InjectableType> allInterfacesOnBaseClasses(Type type)
-//        {
-//            yield return new InjectableType { Attribute = type.GetCustomAttribute<Inject>(), Implementation = implementation, Service = type };
-//            foreach (var interfaceType in type.GetInterfaces())
-//            {
-//                foreach (var interfaceInjectableType in allInterfacesOnBaseClasses(interfaceType))
-//                    yield return interfaceInjectableType;
-//            }
-//        }
-
-//        var interfaceClasses = baseClasses.SelectMany(x => allInterfacesOnBaseClasses(x.Service));
-//        var injectables = baseClasses.Concat(interfaceClasses).Where(x => x.Attribute is not null);
-
-//        return injectables;
-//    }
-//}
+    public static IServiceCollection AddScopes(this IServiceCollection services, IEnumerable<InjectableType> injectableTypes)
+    {
+        foreach (var type in injectableTypes)
+            services.AddScoped(type.Service, type.Implementation);
+        return services;
+    }
+}
